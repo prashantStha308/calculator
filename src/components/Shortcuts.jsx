47e8a174884ac3579c8af89e0896980402b1e28b
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2025-03-11 17:01:52 +0545

    fix: updated desgin for shortcut key icons.

--- src/components/Shortcuts.jsx
+++ src/components/Shortcuts.jsx
@@ -1,3 +1,6 @@
+import { FaArrowUp , FaArrowDown } from "react-icons/fa";
+
+
 const Shortcuts = () => {
 
     const shortcuts= [
@@ -6,21 +9,21 @@ const Shortcuts = () => {
             action: "Expand/Shrink Calculator"
         },
         {
-            keys: [ 'W' , '↑' ],
+            keys: [ 'W' , <FaArrowUp /> ],
             action: "Navigate to Previous Entry"
         },
         {
-            keys:[ 'S' , '↓' ],
+            keys:[ 'S' , <FaArrowDown /> ],
             action: "Navigate to Next Entry"
         }
     ]
 
   return (
-    <ul className="hidden md:grid gap-4 text-sm select-none bg-gray-900/60 px-2 py-5 rounded-xs">
+    <ul className="hidden md:grid gap-4 text-sm select-none bg-gray-800/60 px-2 py-5 rounded-xs">
         {
             shortcuts.map( ( item , index ) =>{
                 return(
-                    <li key={ index }>
+                    <li key={ index } className="flex gap-2 justify-start items-center">
                         {
                             item.keys.map( (key , kIndex) => (
                                 <span key={ kIndex + index } className="shortcut-keys">
@@ -28,7 +31,7 @@ const Shortcuts = () => {
                                 </span>
                             ))
                         }
-                        { item.action }
+                        : { item.action }
                     </li>
                 )
             })
